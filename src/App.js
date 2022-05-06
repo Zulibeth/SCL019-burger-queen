a459ba2ee4925b55552bc20b2cd720ea61e64fae
zulibeth <zulitorres3@gmail.com>
2022-05-05 23:21:40 -0400

    Creacion de data, y primer componente: Break-fast

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import MenuBreakfast from "./breakfast";
+
+function App() {
+  return (
+    <div>
+      <h1>Hola Mundo React</h1>
+      <MenuBreakfast />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/breakfast.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import data from "./data.json"
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+
+
+const MenuBreakfast = () =>{
+    const listOfBreakfast = data.breakFast;
+    return(
+        <div>
+            <h2> Desayunos</h2>
+            {
+                listOfBreakfast.map((item) =>
+                    // <p key={item.id}>{item.item} {item.precio}</p>;
+                    <Box sx={{width: '100%' }}>
+                        <Grid container spacing={2}> 
+                        <Grid item>
+                                <Button variant="contained" size="small">
+                                {item.item} <br/>
+                                {item.precio}
+                                </Button>
+                            </Grid>
+                        </Grid>
+                           
+                    </Box>
+                    )
+            }
+        </div>
+      
+    )
+    
+ 
+    // const desayunos = data.breakFast.name;
+    // const listFood = desayunos.map(item =>
+    //     <li key={item.id}> {item.name} </li>
+    //     );
+    //     return (
+    //         <ul>{listFood}</ul>
+    //     )
+    // const {comidas, setComidas} = React.useState([])
+
+    // React.useEffect(() => {
+    //     console.log("viendo el menu Break-Fast")
+    //     getMenu()
+    // })
+
+    // const getMenu = async () => {
+    //     const menuData = await data.json()
+    //     setComidas(menuData)
+    // }
+    // const listFood = comidas.map((comida) => (
+    //     <li key={comida.id}>{comida.item} - {comida.precio}</li>
+    // ));
+
+    // return (
+    //     <div>
+    //         <button> Break-Fast </button>
+    //         <ul>{listFood}</ul>
+    //     </div>
+    // )
+
+}
+
+export default MenuBreakfast
